d2ae3d4b099097ee5b33b7e7d87f05e5c76a3d08
Mahesh Raghunandanan <mahrag@amazon.com>
2022-01-09 12:58:20 +0530

    CSRF http method explicity added to greeting app

--- app_code/app/src/main/java/org/aws/samples/greeting/GreetingController.java
+++ app_code/app/src/main/java/org/aws/samples/greeting/GreetingController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class GreetingController {
 
-    @RequestMapping("/hello")
+//    @RequestMapping("/hello")
+    @RequestMapping("/hello", method = RequestMethod.GET)
     public String sayHello() {
         return "Hello from AWS ProServe version2";
     }
