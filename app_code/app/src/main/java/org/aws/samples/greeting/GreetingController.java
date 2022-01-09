73163dc3827b48914034d697753edb4f5af01309
Mahesh Raghunandanan <mahrag@amazon.com>
2022-01-09 13:15:03 +0530

    Added import statement for RestMethod

--- app_code/app/src/main/java/org/aws/samples/greeting/GreetingController.java
+++ app_code/app/src/main/java/org/aws/samples/greeting/GreetingController.java
@@ -2,6 +2,7 @@ package org.aws.samples.greeting;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 /**
  * @author MaheshR
@@ -11,11 +12,11 @@ public class GreetingController {
 
 //    @RequestMapping("/hello")
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
-//    public String sayHello() {
-//        return "Hello from AWS ProServe version2";
-//    }
-    public String sayHello(HttpServletRequest request) {
+    public String sayHello() {
         return "Hello from AWS ProServe version2";
     }
+//    public String sayHello(HttpServletRequest request) {
+//        return "Hello from AWS ProServe version2";
+//    }
    
 }
