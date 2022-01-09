aec5473fd7548e7596aea9d26893c6b1e5f5ba4a
Mahesh Raghunandanan <mahrag@amazon.com>
2022-01-10 00:11:41 +0530

    Sonar fxies reverted to reproduce defects

--- app_code/app/src/main/java/org/aws/samples/greeting/GreetingController.java
+++ app_code/app/src/main/java/org/aws/samples/greeting/GreetingController.java
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RestController
 public class GreetingController {
 
-//    @RequestMapping("/hello")
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @RequestMapping("/hello")
+//    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String sayHello() {
         return "Hello from AWS ProServe version2";
     }
